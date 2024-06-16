091bdccef6e54ab05b650a0258bea90589b71c6e
Kim tae-seop <kts3445@naver.com>
2024-06-16 20:58:11 +0900

    first commit

---
+++ src/main/java/com/codingTest_practice/CodingTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codingTest_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTestPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingTest_practice/CodingTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingTest_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
