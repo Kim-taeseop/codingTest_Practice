1d7ac6b0181ed3f470332a160983cfe3cd3baa1c
Kim tae-seop <kts3445@naver.com>
2024-06-22 21:20:49 +0900

    병합 정렬 문제 문제만

---
+++ src/main/java/com/codingTest_practice/sort/mergeSort/P2751_수정렬2.java
@@ -0,0 +1,19 @@
+package com.codingTest_practice.sort.mergeSort;
+
+/*
+- 문제
+N개의 수가 주어졌을 때 이를 오름차순 정렬하는 프로그램 작성
+
+- 입력
+1번째 줄: 수의 개수 N(1<=N<=1000000)
+2번째 줄부터 N개의 줄에 숫자가 주어짐. 수는 절댓값이 1000000보다 작거나 같은 정수, 중복 불가
+
+- 출력
+1번째 줄부터 N개의 줄에 오름차순 정렬 결과를 1줄에 1개씩 출력
+*/
+
+public class P2751_수정렬2 {
+    public static void main(String[] args){
+
+    }
+}
