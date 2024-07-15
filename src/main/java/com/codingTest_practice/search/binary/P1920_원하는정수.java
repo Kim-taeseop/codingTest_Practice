e7a9cc68e464004ff95c4e7661c55ba565767455
Kim tae-seop <kts3445@naver.com>
2024-07-15 20:11:04 +0900

    search

--- src/main/java/com/codingTest_practice/search/binary/P1920_원하는정수.java
+++ src/main/java/com/codingTest_practice/search/binary/P1920_원하는정수.java
@@ -18,36 +18,36 @@ N개의 정수 A[1], A[2], .. A[N] 이 주어질때 이 안에 X라는 정수가
 M개의 줄에 답을 출력. 존재하면 1, 존재하지 않으면 0 출리
 */
 public class P1920_원하는정수 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] A = new int [N];
-        for(int i=0; i<N; i++){
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
         Arrays.sort(A);
         int M = sc.nextInt();
 
-        for(int i=0; i<M; i++){
+        for (int i = 0; i < M; i++) {
             int B = sc.nextInt();
             boolean find = false;
             int start = 0;
-            int end = N-1;
-            while(start <= end){
-                int C = (start + end)/2;
+            int end = N - 1;
+            while (start <= end) {
+                int C = (start + end) / 2;
                 int Cv = A[C];
-                if(Cv > B){
-                    end = C-1;
-                } else if(Cv < B){
-                    start = C+1;
-                }else{
+                if (Cv > B) {
+                    end = C - 1;
+                } else if (Cv < B) {
+                    start = C + 1;
+                } else {
                     find = true;
                     break;
                 }
             }
-            if(find){
+            if (find) {
                 System.out.println(1);
-            }else {
+            } else {
                 System.out.println(0);
             }
         }
